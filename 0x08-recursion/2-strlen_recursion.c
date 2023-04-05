41dc79de3772ab2701aa203be7f8ebe5e4471b4b
Tivah <tivakudzezishiri@gmail.com>
2023-04-05 12:11:49 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * _strlen_recursion - lenth of string
+ * _strlen_recursion - length of string
  * @s:string
  * Return:int
  */
-int _strlen_recursion(char *s);
+int _strlen_recursion(char *s)
 {
 	int i = 0;
 
 	if (*s)
 	{
 		i++;
-		i + = _strlen_recursion(s + 1)
+		i += _strlen_recursion(s + 1);
 
 	}
 	return (i);
