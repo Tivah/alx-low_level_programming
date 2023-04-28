1a1abb56321e1b7430ef16ae1060263af47221ce
Tivah <tivakudzezishiri@gmail.com>
2023-04-28 05:54:46 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,6 @@
-include < stdlib.h >
-#include < string.h >
-#include < stdio.h >
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -20,10 +20,10 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-		printf("[%d] %s\n", h - > len, h - > str);
+		printf("[%d] %s\n", h->len, h->str);
 		}
 		count++;
-		h = h - > next;
+		h = h->next;
 	}
 	return (count);
 }
